25848ad98733973a1417e41f30bcdbdc7ce89171
mreid21 <n01437978@unf.edu>
2023-01-30 16:18:57 -0500

    reset repo

--- server/src/routes/healthcheck.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const healthCheckRouter = express.Router()
-
-healthCheckRouter.get("/", (_, res) => {
-    res.sendStatus(200)
-})
-
-export default healthCheckRouter
